bc568ce6d9c911430c1111b931d234bd5d832d4d
paramesh486 <74412969+paramesh486@users.noreply.github.com>
2020-11-15 02:07:56 +0530

    commit2

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box {
+   constructor(){
+        var options = {
+            restitution :1
+        }   
+        this.body=Bodies.rectangle(200,100,50,50);
+       
+        World.add(world,this.body);
+  }
+
+    display(){
+        var  pos= this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+};
